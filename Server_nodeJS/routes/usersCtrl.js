9fe293a98532351884bc2a4d0b074a317c54e0cb
alex <alexandre.blondel@viacesi.fr>
2019-11-12 17:00:29 +0100

    start connexion in js

---
+++ Server_nodeJS/routes/usersCtrl.js
@@ -0,0 +1,14 @@
+//Imports
+var bcrypt  = require('bcrypt');
+var jwt     = require('jsonwebtoken');
+
+//Routes 
+module.exports = {
+    register: function(req, res){
+        var email = req.bo
+
+    },
+    login: function(req, res){
+
+    }
+}
--- Server_nodeJS/serveur.js
+++ Server_nodeJS/serveur.js
@@ -1,15 +1,22 @@
 //Import
 var express = require('express');
+var bodyParser = require('body-parser');
+var apiRouter = require('./apiRouter').router;
 
 //Instantite server
 var server = express();
 
+//Body Parser configuration
+server.use(bodyParser.urlencoded({ extended: true}));
+server.use(bodyParser.json());
 
 //configure routes
 server.get('/',function (req, res) {
     res.setHeader('Content-Type','text/html');
     res.status(200).send('<h1>Bonjour sur mon serveur</h1>');
 });
+
+server.use('/api/', apiRouter);
 
 //Lauch server
 server.listen(8000, function () {
