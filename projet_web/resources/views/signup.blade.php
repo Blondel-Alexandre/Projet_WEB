c08502005dff319f1d9fc58f75c90fdf1653a6d0
unknown <Administrateur@ARPEDAPRET05.NextGen.Cesi>
2019-11-13 15:29:05 +0100

    oui

--- projet_web/resources/views/signup.blade.php
+++ projet_web/resources/views/signup.blade.php
@@ -1,19 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends ('Layout')
+    @section('contenu')
 
-    <title>Laravel</title>
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <link href="{{asset('css/header.css')}}" rel="stylesheet" />
-    <link crossorigin="anonymous" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" rel="stylesheet">
-    <!-- Styles -->
+<!-- Styles -->
     <style>
 
 form {
@@ -25,28 +13,28 @@ form {
     border: 1px solid #CCC;
     border-radius: 1em;
   }
-  
+
 form div + div {
     margin-top: 1em;
   }
-  
+
 label {
     /* Pour être sûrs que toutes les étiquettes ont même taille et sont correctement alignées */
     display: inline-block;
     width: 400px;
     text-align: left;
   }
-  
+
 input:focus, textarea:focus {
     /* Pour souligner légèrement les éléments actifs */
     border-color: #000;
   }
-  
+
 .button {
     /* Pour placer le bouton à la même position que les champs texte */
     padding-left: 90px; /* même taille que les étiquettes */
   }
-  
+
 button {
     /* Cette marge supplémentaire représente grosso modo le même espace que celui
        entre les étiquettes et les champs texte */
@@ -54,10 +42,6 @@ button {
   }
     </style>
 
-
-</head>
-
-<body>
     <form action="" method="post">
         <div>
             <label for="name">Nom :</label>
@@ -97,4 +81,5 @@ button {
         </div>
 
     </form>
-</body>
+
+@endsection
