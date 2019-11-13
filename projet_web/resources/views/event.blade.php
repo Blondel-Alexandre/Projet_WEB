b11c113675864282c2817856aff5ef712d158b1a
bastien <bastien.seillier@viacesi.fr>
2019-11-13 14:48:17 +0100

    event

--- projet_web/resources/views/event.blade.php
+++ projet_web/resources/views/event.blade.php
@@ -15,5 +15,15 @@
         <p class="mot"><a class="motcle2" href="lienvente2.html"> event 2 passé </a><span class="desc">toujours event bla bla duhf cheh fetrtui dhu hd</span> </p>
     </div>
 
+    <div id="vente3">
+        <img src="pictures/event1.png" />
+        <p><a class="motcle" href="lienvente1.html"> event 3 passé </a></br> <span class="desc">premier event tmtc tro cool enfaite hate de la suite blablabla .....</span> </p>
+    </div>
+
+    <div id="vente4">
+        <img src="pictures/event1.png" />
+        <p class="mot"><a class="motcle2" href="lienvente2.html"> event 4 passé </a><span class="desc">toujours event bla bla duhf cheh fetrtui dhu hd</span> </p>
+    </div>
+
 
 @endsection
