0f27170baa149b0e92df1089fa0935da3f35c543
bastien <bastien.seillier@viacesi.fr>
2019-11-14 20:10:23 +0100

    email contact

--- projet_web/resources/views/contact.blade.php
+++ projet_web/resources/views/contact.blade.php
@@ -9,4 +9,8 @@
     <small>Service & appel gratuit de 9h à 12h et de 14h à 17h du lundi au vendredi.</small>
     <p>"0 800 054 568</p>
 
+    <h2>Envoyez-nous un mail</h2>
+    <p>Une question prècise ? Ecrivez à CESI, à une école ou à un campus</p>
+
 @endsection
+
