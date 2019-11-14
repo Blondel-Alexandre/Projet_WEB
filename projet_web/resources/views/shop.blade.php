13fd222b8d7eefc6a67067264224ad4a44001ac6
Simon <simon.delayen@viacesi.fr>
2019-11-14 16:39:47 +0100

    boutique responsive

--- projet_web/resources/views/shop.blade.php
+++ projet_web/resources/views/shop.blade.php
@@ -1,14 +1,19 @@
 @extends('Layout')
-<link href="{{asset('css/boutique.css')}}" rel="stylesheet" />
+ <link href="{{asset('css/boutique.css')}}" rel="stylesheet" />
 
 @section ('contenu')
     <nav>
         <h1>boutique</h1>
-
         <form class="form-inline my-2 my-lg-0">
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            <a class="backet" href="backet" ><i class="fas fa-shopping-basket"></i></a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Arras</a>
+                <a class="dropdown-item" href="#">Rouen</a>
+                <a class="dropdown-item" href="#">Strasbourg</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Autre campus</a>
+            </div>
         </form>
 
     </nav>
@@ -16,22 +21,22 @@
 
 
 
-    <div id="vente1">
+    <div id="vente">
         <img src="pictures/boutique.jpg" />
         <p><a class="motcle" href="/link1shop"> vente 1 </a></br> <span class="desc">premier event tmtc tro cool enfaite hate de la suite blablabla .....</span> </p>
     </div>
-
-    <div id="vente2">
+ <hr>
+    <div id="vente">
         <img src="pictures/boutique.jpg" />
         <p><a class="motcle" href="link1shop2"> vente 2 </a><span class="desc">toujours event bla bla duhf cheh fetrtui dhu hd</span> </p>
     </div>
-
-    <div id="vente3">
+<hr>
+    <div id="vente">
         <img src="pictures/boutique.jpg" />
         <p><a class="motcle" href="link1shop3"> vente 3 </a></br> <span class="desc">premier event tmtc tro cool enfaite hate de la suite blablabla .....</span> </p>
     </div>
-
-    <div id="vente4">
+<hr>
+    <div id="vente">
         <img src="pictures/boutique.jpg" />
         <p><a class="motcle" href="link1shop4"> vente 4 </a><span class="desc">toujours event bla bla duhf cheh fetrtui dhu hd</span> </p>
     </div>
