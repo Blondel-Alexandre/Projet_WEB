2ff5a5f6139200eec2414d38b0ae4442db515492
Simon <simon.delayen@viacesi.fr>
2019-11-14 09:36:54 +0100

    debordement

--- projet_web/resources/views/signup.blade.php
+++ projet_web/resources/views/signup.blade.php
@@ -1,48 +1,10 @@
 @extends ('Layout')
+
+<link href="{{asset('css/styleSignUp.css')}}" rel="stylesheet" />
+
     @section('contenu')
 
-<!-- Styles -->
-    <style>
 
-form {
-    /* Uniquement centrer le formulaire sur la page */
-    margin: 0 auto;
-    margin-top: 2px;
-    width: 400px;
-    /* Encadré pour voir les limites du formulaire */
-    padding: 1em;
-    border: 1px solid #CCC;
-    border-radius: 1em;
-  }
-
-form div + div {
-    margin-top: 1em;
-  }
-
-label {
-    /* Pour être sûrs que toutes les étiquettes ont même taille et sont correctement alignées */
-    display: inline-block;
-    width: 400px;
-    text-align: left;
-    padding: 0px 0px 5px 0px;
-  }
-
-input:focus, textarea:focus {
-    /* Pour souligner légèrement les éléments actifs */
-    border-color: #000;
-  }
-
-.button {
-    /* Pour placer le bouton à la même position que les champs texte */
-    padding-left: 90px; /* même taille que les étiquettes */
-  }
-
-button {
-    /* Cette marge supplémentaire représente grosso modo le même espace que celui
-       entre les étiquettes et les champs texte */
-    margin-left: .5em;
-  }
-    </style>
 
     <form action="" method="post">
         <div>
@@ -72,7 +34,7 @@ button {
 
         <div>
             <label for="password">Mot de passe (Contenant 1 Maj, 1 Min, 1 Chiffre minimum ) :</label>
-            <input type="password" id="password" name="user_password">
+            <input type="text" id="password" name="user_password">
         </div>
 
         <div>
