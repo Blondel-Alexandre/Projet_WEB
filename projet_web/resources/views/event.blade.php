e3ffe1ee3849a63454cd402c0bb1af0f011453f3
Simon <simon.delayen@viacesi.fr>
2019-11-18 10:50:22 +0100

    event

--- projet_web/resources/views/event.blade.php
+++ projet_web/resources/views/event.blade.php
@@ -6,75 +6,94 @@
     <form action="/api/addComment" method="post">
 
         <h1>Evènement</h1>
-        <a class="btn btn-secondary" href="/inputEvent" role=button">ajouter un évènement<a/>
-            <hr>
+        <a class="btn btn-secondary" href="/inputEvent" role="button">ajouter un évènement</a>
+        <hr>
             <div id="eventpast">
                 <img src="pictures/event1.png" width="626" height="469" class="responsive" />
                 <p>
-                    <a
-                        class="keypast" href="linkeventpast"> event 1 passé
-                    </a>
-                    </br>
-                    <span
-                        class="desc">premier event tmtc tro cool enfaite hate de la suite blablabla .....
-                     </span>
-                    <a href="#" class="like"><i class="far fa-thumbs-up"></i></a>
-                    <br/>
-                    <a class="btn btn-info" href="/addphoto2" role="button">Photo</a>
-                    <br/>
-                <div>
+                    
+                    <div>
+                        <a
+                            class="keypast" href="linkeventpast"> event 1 passé
+                        </a>
+                        <br>
+                        <span class="desc">
+                            premier event tmtc tro cool enfaite hate de la suite blablabla .....
+                        </span>
+                        <a href="#" class="like"><i class="far fa-thumbs-up"></i></a>
+                        <a class="btn btn-info" href="/addphoto2" role="button">Photo</a>
+                    </div>
+                   
+                <div class="add">
                     <label for="comment">Commentaire</label>
                     <input type="text" id="comment" name="tppComment_comment">
                     <button type="submit">Envoyer</button>
                 </div>
                 </p>
-                <div>
 
-                </div>
             </div>
+
             <hr>
+
             <div id="eventfuture">
                 <img src="pictures/event1.png" width="626" height="469" class="responsive" />
                 <p class="mot">
                     <a
                         class="keyfuture" href="eventsignin"> event 1 futur
                     </a>
-                    <span
-                        class="desc">toujours event bla bla duhf cheh fetrtui dhu hd
-                <br/>
-                   <a class="btn btn-success" href="/eventsignin" role="button">S'inscrire</a>
-                         <a class="btn btn-warning" href="/" role="button">Recuperer les inscrits</a>
-            </span>
-                </p>
+                    <br>
+                    <span class="desc">
+                        toujours event bla bla duhf cheh fetrtui dhu hd
+                    </span>
+                <div>
+                    <a class="btn btn-success" href="/eventsignin" role="button">S'inscrire</a>
+                    <a class="btn btn-warning" href="/" role="button">Recuperer les inscrits</a>
+                </div>
             </div>
+
             <hr>
+
             <div id="eventpast">
                 <img src="pictures/event1.png" width="626" height="469" class="responsive" />
                 <p>
-                    <a
-                        class="keypast" href="linkeventpast2"> event 2 passé
-                    </a>
-                    </br>
-                    <span
-                        class="desc">premier event tmtc tro cool enfaite hate de la suite blablabla .....
-            </span>
-                    <a href="#" class="like"><i class="far fa-thumbs-up"></i></a>
-                    <br/>
-                    <a class="btn btn-info" href="/addphoto2" role="button">Photo</a>
+                    
+                    <div>
+                        <a
+                            class="keypast" href="linkeventpast"> event 1 passé
+                        </a>
+                        <br>
+                        <span class="desc">
+                            premier event tmtc tro cool enfaite hate de la suite blablabla .....
+                        </span>
+                        <a href="#" class="like"><i class="far fa-thumbs-up"></i></a>
+                        <a class="btn btn-info" href="/addphoto2" role="button">Photo</a>
+                    </div>
+                   
+                <div class="add">
+                    <label for="comment">Commentaire</label>
+                    <input type="text" id="comment" name="tppComment_comment">
+                    <button type="submit">Envoyer</button>
+                </div>
                 </p>
+
             </div>
+
             <hr>
+
             <div id="eventfuture">
                 <img src="pictures/event1.png" width="626" height="469" class="responsive" />
                 <p class="mot">
                     <a
-                        class="keyfuture" href="linkeventfutur2"> event 2 futur
+                        class="keyfuture" href="eventsignin"> event 1 futur
                     </a>
-                    <span
-                        class="desc">toujours event bla bla duhf cheh fetrtui dhu hd
-                <br/>
-                    <a class="btn btn-success" href="/eventsignin2" role="button">S'inscrire</a>
-            </span> </p>
+                    <br>
+                    <span class="desc">
+                        toujours event bla bla duhf cheh fetrtui dhu hd
+                    </span>
+                <div>
+                    <a class="btn btn-success" href="/eventsignin" role="button">S'inscrire</a>
+                    <a class="btn btn-warning" href="/" role="button">Recuperer les inscrits</a>
+                </div>
             </div>
     </form>
 
